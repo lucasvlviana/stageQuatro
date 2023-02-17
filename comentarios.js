9e863aad23af1e466703c12adb70638ddc65c015
Lucas Viana <lucasvitorlv@gmail.com>
2023-02-17 11:26:54 -0300

    aprendendo sobre comentarios

---
+++ comentarios.js
@@ -0,0 +1,9 @@
+//Aqui eu tenho um comentário em linha
+
+/*Aqui eu tenho um comentário multi
+linha
+
+o código abaixo escreve alguma mensagem no devtools*/
+
+console.log('Bem vindos ao Starter')
+console.log('Não iria ignorar')
