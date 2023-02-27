e04648bf51a39c53e557753b0e18e18bd8e6e8cb
Lucas Viana <lucasvitorlv@gmail.com>
2023-02-27 16:02:51 -0300

    aulas sobre funções

---
+++ functions.js
@@ -0,0 +1,11 @@
+// criar um aplicativo de frases motivacionais
+
+// console.log('Estudar é muito bom');
+// console.log('Paciência e persistência');
+// console.log('Revisão é a mãe do aprendizado');
+
+function createPhrases() {
+    console.log('Estudar é muito bom');
+    console.log('Paciência e persistência');
+    console.log('Revisão é a mãe do aprendizado');
+}
