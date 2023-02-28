2c3655c5ea15a2e73e6c74fa5d98a810a64e7ec1
Lucas Viana <lucasvitorlv@gmail.com>
2023-02-28 10:05:07 -0300

    subindo arquivos da aula

--- functions.js
+++ functions.js
@@ -1,11 +1,11 @@
-// criar um aplicativo de frases motivacionais
-
-// console.log('Estudar é muito bom');
-// console.log('Paciência e persistência');
-// console.log('Revisão é a mãe do aprendizado');
-
-function createPhrases() {
-    console.log('Estudar é muito bom');
-    console.log('Paciência e persistência');
-    console.log('Revisão é a mãe do aprendizado');
+const sum = function (number1, number2){
+    total = number1 + number2
+    return total
 }
+
+let number1 = 34
+let number2 = 25
+
+console.log('O número 1 é ${number1}')
+console.log('O número 2 é ${number2}')
+console.log('A soma é ${sum(number1, number2)}')
---
+++ phrases.js
@@ -0,0 +1,15 @@
+// criar um aplicativo de frases motivacionais
+
+// console.log('Estudar é muito bom');
+// console.log('Paciência e persistência');
+// console.log('Revisão é a mãe do aprendizado');
+
+function createPhrases() {
+    console.log('Estudar é muito bom');
+    console.log('Paciência e persistência');
+    console.log('Revisão é a mãe do aprendizado');
+}
+
+createPhrases();
+
+console.log('Fim do programa');
