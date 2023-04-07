ddbcfd9bc65cfd42001e95fa0a3a38021f50590b
lucasvlviana <70477354+lucasvlviana@users.noreply.github.com>
2023-04-07 14:29:27 -0300

    Delete comentarios.js

--- comentarios.js
+++
@@ -1,9 +0,0 @@
-//Aqui eu tenho um comentário em linha
-
-/*Aqui eu tenho um comentário multi
-linha
-
-o código abaixo escreve alguma mensagem no devtools*/
-
-console.log('Bem vindos ao Starter')
-console.log('Não iria ignorar')
